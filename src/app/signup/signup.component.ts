8b107bde93cfa71480304d81e18927dfd70f11d3
cavanosa <cavanosa@cavanosa.cav>
2021-12-08 14:34:35 +0100

    signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Auth } from 'aws-amplify';
 
 @Component({
   selector: 'app-signup',
@@ -11,10 +13,44 @@ export class SignupComponent implements OnInit {
   userName: string;
   nickname: string;
 
+  verifying: boolean = false;
+  verifyCode: string;
 
-  constructor() { }
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onRegister(): void {
+    const user = {
+      username: this.email, // OJO es el email
+      password: this.password,
+      attributes: {
+        email: this.email,
+        given_name: this.userName,
+        nickname: this.nickname
+      }
+    }
+    Auth.signUp(user).then( data => {
+      this.verifying = true;
+      alert("te hemos enviado un correo con el código de activación");
+    }).catch( err => {
+      alert(err.message || JSON.stringify(err))
+    });
+  }
+
+  onVerify(): void {
+    Auth.confirmSignUp(this.email, this.verifyCode, {forceAliasCreation: true}).then( data => {
+      this.verifying = false;
+      alert("tu correo ha sido verificado");
+      this.router.navigate(['']);
+    })
+    .catch( err => {
+      alert(err.message || JSON.stringify(err))
+    });
   }
 
 }

4952cae9b42409a01d61f81870e549dcd52511eb
cavanosa <cavanosa@cavanosa.cav>
2021-12-09 19:44:51 +0100

    signout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import Auth from '@aws-amplify/auth';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  attributes: any;
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onLogout(): void {
+    Auth.signOut().then( () => {
+      this.router.navigate(['/']);
+    }).catch( err => {
+      alert(err.message || JSON.stringify(err));
+    })
+  }
+
+  getAttributes(): void {
+    Auth.currentUserInfo().then( user => {
+      this.attributes = user.attributes;
+      // alert(JSON.stringify(this.attributes));
+    }).catch( err => {
+      alert(err.message || JSON.stringify(err));
+    })
   }
 
 }
